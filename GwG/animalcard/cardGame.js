d611c4237735af2b43dc4c68d157e6472a3f2c84
JimAlanMcB <alancode@gmail.com>
2018-01-14 13:45:42 -0700

    Added a stop function for sound, and made it where once you click one or the other it stops the audio

--- GwG/animalcard/cardGame.js
+++ GwG/animalcard/cardGame.js
@@ -7,6 +7,26 @@ var audioR = new Audio("sounds/rhino.mp3");
 var audioH = new Audio("sounds/hippo.mp3");
 var audioHy = new Audio("sounds/hyena.mp3");
 
+
+
+Audio.prototype.stop = function () {
+    this.pause();
+    this.currentTime = 0.0;
+}
+
+function stopAudio() {
+
+    audioC.stop();
+    audioE.stop();
+    audioG.stop();
+    audioZ.stop();
+    audioGi.stop();
+    audioR.stop();
+    audioH.stop();
+    audioHy.stop();
+
+}
+
 var cheetahCard = document.getElementById("cheetah");
 var elephantCard = document.getElementById("elephant");
 var gazelleCard = document.getElementById("gazelle");
@@ -16,28 +36,37 @@ var rhinoCard = document.getElementById("rhino");
 var hippoCard = document.getElementById("hippo");
 var hyenaCard = document.getElementById("hyena");
 
+// currently adding each audio a stop function. May be able to use a for loop...make a function for it.  
 
 cheetahCard.addEventListener("click", function () {
+    stopAudio();
     audioC.play();
 });
 elephantCard.addEventListener("click", function () {
+    stopAudio();
     audioE.play();
 });
 gazelleCard.addEventListener("click", function () {
+    stopAudio();
     audioG.play();
 });
 zebraCard.addEventListener("click", function () {
+    stopAudio();
     audioZ.play();
 });
 giraffeCard.addEventListener("click", function () {
+    stopAudio();
     audioGi.play();
 });
 rhinoCard.addEventListener("click", function () {
+    stopAudio();
     audioR.play();
 });
 hippoCard.addEventListener("click", function () {
+    stopAudio();
     audioH.play();
 });
 hyenaCard.addEventListener("click", function () {
+    stopAudio();
     audioHy.play();
 });
