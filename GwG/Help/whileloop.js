c123fffb9f141da1237985d27fd701afeea5b6b3
JimAlanMcB <alancode@gmail.com>
2018-03-07 00:05:10 -0700

    adding whileloop js edits

--- GwG/Help/whileloop.js
+++ GwG/Help/whileloop.js
@@ -2,31 +2,51 @@
  * Programming Quiz: JuliaJames (4-1)
  */
 
-var x = 0;
-while (x <= 19) {
-    x = x + 1;
+// var x = 0;
+// while (x <= 19) {
+//     x = x + 1;
 
-    var Julia = x % 3 == 0 && x % 5 == 0 ? console.log("JuliaJames")
-    : x % 5 == 0 ? console.log("James") 
-    : x % 3 == 0 ? console.log("Julia") 
-    : console.log(x);
-}
+//     var Julia = x % 3 == 0 && x % 5 == 0 ? console.log("JuliaJames")
+//     : x % 5 == 0 ? console.log("James") 
+//     : x % 3 == 0 ? console.log("Julia") 
+//     : console.log(x);
+// }
 
 
-  
-    // var James = x % 5 == 0 ? console.log("James") : console.log(x);
-    // var JuliaJames = x % 3 == 0 && x % 5 == 0 ? console.log("JuliaJames") : console.log(x);
+
+// var James = x % 5 == 0 ? console.log("James") : console.log(x);
+// var JuliaJames = x % 3 == 0 && x % 5 == 0 ? console.log("JuliaJames") : console.log(x);
+
+// console.log(Julia);
+// console.log(JuliaJames);
+// console.log(James);
+
+// var isDivisible = x % 3 == 0 ? x = "Julia" : x; 
+
+//console.log(x);
+
+// check divisibility
+// print Julia, James, or JuliaJames
+
+
+// increment x
+
+////////// LIVE HELP QUESTION AND ANSWER BELOW
+
+// var x = 0;
+
+// while (x < 19) {
+//     x += 1;
+//     if (x % 3 == 0 && x % 5 == 0)
+//         console.log("JuliaJames");
+//     else if (x % 3 == 0) 
+//         console.log("Julia");
+    
+//     else if (x % 5 == 0) 
+//         console.log("James");
  
-    // console.log(Julia);
-    // console.log(JuliaJames);
-    // console.log(James);
+//     else
+//         console.log(x);
     
-    // var isDivisible = x % 3 == 0 ? x = "Julia" : x; 
     
-        //console.log(x);
-
-    // check divisibility
-    // print Julia, James, or JuliaJames
-
-
-    // increment x
+// }
